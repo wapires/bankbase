7dabfd4eef8d67b9a84151cbdb4eedd4ea4bc25e
David Green <dgreen@uab.edu>
2021-12-13 14:30:03 -0600

    Refactored main into TestBank class

--- src/main/java/edu/uab/ee333/bankinheritance/CheckingAccount.java
+++ src/main/java/edu/uab/ee333/bankinheritance/CheckingAccount.java
@@ -18,8 +18,8 @@ public class CheckingAccount extends BankAccount {
   }
 
   /**
-   * toString - convert to a string representation showing the name of the checking account and the
-   * present balance
+   * toString - convert to a string representation showing the name of the
+   * checking account and the present balance
    *
    * @return a string representation of a checking account
    */
@@ -43,41 +43,11 @@ public class CheckingAccount extends BankAccount {
     }
   }
 
-  /** Assess a 150 cent fee each month */
+  /**
+   * Assess a 150 cent fee each month
+   */
   @Override
   public void assessMonthlyFee() {
     balance -= 150;
-  }
-
-  /**
-   * Small program to test this class
-   *
-   * @param args from command line - unused
-   */
-  public static void main(String[] args) {
-
-    CheckingAccount mine = new CheckingAccount("David Green", 200000);
-    System.out.println("The account has " + mine.getBalance());
-    System.out.println(mine);
-
-    mine.deposit(1000);
-    System.out.println(mine);
-
-    mine.withdraw(200000);
-    System.out.println(mine);
-
-    // trying to remove too much
-    boolean status = mine.withdraw(200000);
-    System.out.println(mine);
-    System.out.println("Withdraw returned " + status);
-
-    System.out.println(mine.clearCheck(1));
-    System.out.println(mine);
-
-    System.out.println(mine.clearCheck(10000));
-    System.out.println(mine);
-
-    mine.assessMonthlyFee();
-    System.out.println(mine);
   }
 }
